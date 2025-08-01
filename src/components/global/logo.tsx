0b2ac6f6c36a33200916e3dc4f5b00ee2b54de3e
Radhey Mugdal <mugdalradhey@gmail.com>
2025-08-01 21:57:47 +0530

    feat: enhance pricing components with user session handling and UI improvements

--- src/components/global/logo.tsx
+++ src/components/global/logo.tsx
@@ -5,11 +5,11 @@ import { LuWaves } from "react-icons/lu";
 const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="flex gap-2 justify-center items-center">
+      <div className="flex gap-1 justify-center items-center">
         <span>
           <LuWaves className=" size-6 text-primary" />
         </span>
-        <h1 className=" font-bold  text-2xl">Wavely</h1>
+        <h1 className=" font-bold  text-lg">Wavely</h1>
       </div>
     </Link>
   );
