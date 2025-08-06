44707cb2006fd96a5257388c6aff320999e729c9
Radhey Mugdal <mugdalradhey@gmail.com>
2025-08-06 16:24:57 +0530

    fix: authorize user in payment-complete page

--- src/app/(main)/payment-complete/page.tsx
+++ src/app/(main)/payment-complete/page.tsx
@@ -1,13 +1,20 @@
 import ErrorState from "@/components/global/error-state";
 import Loader from "@/components/global/loader";
+import { auth } from "@/lib/auth";
 import SuccessView from "@/modules/pricing/ui/views/success-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 
-const SuccessPage = () => {
+const SuccessPage = async () => {
+    const session = await auth.api.getSession({ headers: await headers() });
+    if (!session) {
+        redirect("/sign-in");
+    }
     const queryClient = getQueryClient()
     void queryClient.prefetchQuery(trpc.pricing.getCurrentSubscription.queryOptions())
 
