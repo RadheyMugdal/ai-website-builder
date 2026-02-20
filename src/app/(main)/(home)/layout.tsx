8192c27f92f530819c7b4c2d6141e9d50820641a
Radhey Mugdal <mugdalradhey@gmail.com>
2026-02-20 12:46:34 +0530

    feat: add @vercel/analytics integration and update dependencies

--- src/app/(main)/(home)/layout.tsx
+++ src/app/(main)/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import Header from "@/modules/chat/ui/components/header";
 import { headers } from "next/headers";
+import { Analytics } from "@vercel/analytics/next"
 import React from "react";
 
 type Props = {
@@ -11,6 +12,7 @@ const layout = async ({ children }: Props) => {
   const session = await auth.api.getSession({ headers: await headers() });
   return (
     <div className="flex flex-col min-h-screen max-w-7xl  mx-auto">
+      <Analytics/>
       <Header isLoggedIn={!!session} />
       <main className="flex-1 flex flex-col">{children}</main>
     </div>
